56fb83a4eaa49409c87fdbe05be693931c4e4564
Wendy Argente <wargente_10@outlook.com>
2017-09-05 00:19:47 -0500

    He "peinado" el código
    He comentado las funciones del archivo para tener un mejor
    entendimiento, además de que he establecido como una variable $GLOBALS,
    a la instancia de System, de esta forma es posible manejar la misma
    instancia dentro de todas las clases.

--- index.php
+++ index.php
@@ -4,23 +4,135 @@
 # @Email:  wendyargente@nube.unadmexico.mx
 # @Project: Turing
 # @Last modified by:   wendylu
-# @Last modified time: 2017-08-31T20:07:21-05:00
+# @Last modified time: 2017-09-05T00:18:41-05:00
 # @License: MIT
+/*
 
-
-
-
-include 'system/core/System.php';
-
-//
-// Nombra al controlador principal que maneja las peticiones
-//
+ * ------------------------------------------------------
+ * CONTROLADOR PRINCIPAL
+ * ------------------------------------------------------
+ *
+ * Se nombra al controlador por defecto, es decir, este
+ * será el controlador que será llamado cuando no se
+ * haya realizado la petición de ningun controlador,
+ * cuando la URL se encuentre vacía.
+ * ------------------------------------------------------
+ */
 define('DEFAULT_CONTROLLER', 'inicio');
 
 
-//
-// Crea una instancia de la clase sistema,
-// y llama al método init()
-//
-$system = new System();
-$system->init();
+/*
+ * ------------------------------------------------------
+ * RESOLUCIÓN DE PETICIONES
+ * ------------------------------------------------------
+ *
+ * Comienza la resolución del URI, toma el recurso y lo
+ * dividen en varias partes hasta poder definir cada una
+ * de ellas.
+ */
+$basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, - 1)).'/';
+$uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+$routes = explode('/', $uri);
+
+
+/*
+ * ------------------------------------------------------
+ * DEFINE LAS PETICIONES
+ * ------------------------------------------------------
+ *
+ * Si el primer índice está definido, se definirá como
+ * el controlador al que se está llamando, si no, entonces
+ * se llamará al controlador que se a definido por defecto
+ * para recibir las peticiones.
+ */
+isset($routes[1])
+? $controller = $routes[1]
+: $controller = DEFAULT_CONTROLLER;
+
+
+/*
+ * ------------------------------------------------------
+ * ¿Hay un método solicitado?
+ * ------------------------------------------------------
+ */
+isset($routes[2])
+? $method = $routes[2]
+: $method = "index";
+
+/*
+ * ------------------------------------------------------
+ * ¿Se recibe un parámetro?
+ * ------------------------------------------------------
+ */
+isset($routes[3])
+? $param = $routes[3]
+: $param = "";
+
+
+/*
+ * ------------------------------------------------------
+ * DEFINICIÓN DE RUTAS CONSTANTES
+ * ------------------------------------------------------
+ *
+ * Ya que se tienen las variables claves, se pueden definir
+ * las constantes que son usadas durante todo el programa.
+ */
+// Nombre del servidor
+define('SERVER_NAME', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME']);
+
+// Ruta base del proyecto
+define('BASEPATH', SERVER_NAME.$basepath);
+
+// Ruta base del proyecto, frente del index.php/
+define('BASE_URL', BASEPATH.'index.php');
+
+// Frente del directorio de la aplicación
+define('APPPATH', SERVER_NAME.$basepath.'app/');
+
+// Frente de los archivos estáticos
+define('STATICPATH', APPPATH.'assets/');
+
+// Frente del directorio de las vistas
+define('VIEWPATH', APPPATH.'views/');
+
+// Frente del directorio del sistema
+define('SYSPATH', SERVER_NAME.$basepath.'system/');
+
+
+// echo "SERVER_NAME: ".SERVER_NAME."<br>";
+// echo "base_url: ".BASE_URL."<br>";
+// echo "basepath: ".BASEPATH."<br>";
+// echo "apppath: ".APPPATH."<br>";
+// echo "viewpath: ".VIEWPATH."<br>";
+// echo "SYSPATH: ".SYSPATH."<br>";
+// echo "STATICPATH: ".STATICPATH."<br>";
+
+/*
+ * ------------------------------------------------------
+ * Carga las funciones comúnes
+ * ------------------------------------------------------
+ */
+require_once 'system/core/Common.php';
+
+
+/*
+ * ------------------------------------------------------
+ * Carga las clases principales
+ * ------------------------------------------------------
+ */
+loadClasses('system/core/');
+
+/*
+ * ------------------------------------------------------
+ * CARGA EL SISTEMA
+ * ------------------------------------------------------
+ *
+ * Una vez que está todo listo...
+ *
+ * Aquí vamos... :)
+ */
+require_once 'system/core/System.php';
+
+$GLOBALS['system'] = new System\Core\System();
+
+$system->controller($controller, $method, $param);
